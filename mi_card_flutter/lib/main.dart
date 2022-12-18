6677deac19b16381de984369ab1a637262ffa76c
LuccaMoraes <lucas.pk16@outlook.com>
2022-12-17 22:23:37 -0300

    android build was too old, rebuilded version

--- mi_card_flutter/lib/main.dart
+++ mi_card_flutter/lib/main.dart
@@ -1,12 +1,18 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(
-    MaterialApp(
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
-        backgroundColor: Colors.teal,
+        backgroundColor: Colors.red,
         body: Container(),
       ),
-    ),
-  );
+    );
+  }
 }
