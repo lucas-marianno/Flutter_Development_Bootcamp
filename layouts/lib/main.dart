091f3604f4c977d5a5502c35702dd301c3693ce3
LuccaMoraes <lucas.pk16@outlook.com>
2023-02-22 21:21:17 -0300

    layouts exercise

--- layouts/lib/main.dart
+++ layouts/lib/main.dart
@@ -13,12 +13,35 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       home: SafeArea(
         child: Scaffold(
+          backgroundColor: Colors.teal,
           //appBar: AppBar(title: Text('App bar'),),
-          body: Center(
-            child: Container(
-                width: 100,
-                color: Colors.red,
-              )
+          body: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Container(
+                  width: 100,
+                  color: Colors.red,
+                ),
+              Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Container(
+                      width: 100,
+                      height: 100,
+                      color: Colors.yellow,
+                    ),
+                  Container(
+                    width: 100,
+                    height: 100,
+                    color: Colors.green,
+                  ),
+                ],
+              ),
+              Container(
+                  width: 100,
+                  color: Colors.blue,
+                ),
+            ],
           ),
         ),
       ),
