24314305e53bec578d592ec1462f2f5a25c324d3
Lucas Moraes <lucas.pk16@outlook.com>
2023-03-29 17:47:39 -0300

    image.asset(path)

--- dicee_flutter/lib/main.dart
+++ dicee_flutter/lib/main.dart
@@ -18,6 +18,24 @@ void main() {
 class DicePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Center(
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.all(16),
+              child: Image.asset('images/dice1.png'),
+            ),
+          ),
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.all(16),
+              child: Image.asset('images/dice2.png'),
+            ),
+          )
+        ],
+      ),
+    );
   }
 }
