36067724482add52d0f24900b3b31412808094ca
LuccaMoraes <lucas.pk16@outlook.com>
2023-03-29 21:31:18 -0300

    8ball completed

--- magic_8_ball_flutter/lib/main.dart
+++ magic_8_ball_flutter/lib/main.dart
@@ -1,7 +1,45 @@
+import 'dart:math';
 import 'package:flutter/material.dart';
 
 void main() => runApp(
       MaterialApp(
-        home: null,
+        debugShowCheckedModeBanner: false,
+        home: HomePage(),
       ),
     );
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  String ball = newBall();
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.white,
+          foregroundColor: Colors.black,
+          elevation: 0,
+          title: Center(child: Text('ASK ME ANYTHING!')),
+        ),
+        body: Center(
+          child: TextButton(
+            onPressed: () {
+              setState(() {
+                ball = newBall();
+              });
+            },
+            child: Image.asset('images/ball' + ball + '.png'),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+String newBall() => '${Random().nextInt(5) + 1}';
