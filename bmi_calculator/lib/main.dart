76162f7e52c8749afd0b4224143013060aaf8452
Lucas Moraes <lucas.pk16@outlook.com>
2023-05-11 13:29:56 -0300

    wip

--- bmi_calculator/lib/main.dart
+++ bmi_calculator/lib/main.dart
@@ -8,6 +8,7 @@ class BMICalculator extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: InputPage(),
     );
   }
