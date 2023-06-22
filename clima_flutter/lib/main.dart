601029820c6c608bccfef5befd82127c71cdb9ba
Lucas Moraes <lucas.pk16@outlook.com>
2023-06-22 11:27:31 -0300

    clima_flutter first (flutter 3 migrated)

---
+++ clima_flutter/lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:clima_flutter/screens/loading_screen.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData.dark(),
+      home: const LoadingScreen(),
+    );
+  }
+}
---
+++ clima_flutter/lib/screens/loading_screen.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class LoadingScreen extends StatefulWidget {
+  const LoadingScreen({super.key});
+
+  @override
+  State<LoadingScreen> createState() => _LoadingScreenState();
+}
+
+class _LoadingScreenState extends State<LoadingScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {
+            //Get the current location
+          },
+          child: const Text('Get Location'),
+        ),
+      ),
+    );
+  }
+}
