16e23d77211c98c2cebb4f8e200651f8c0c489b1
lucas-marianno <lucas.pk16@outlook.com>
2023-07-06 22:05:39 -0300

    bitcoin ticker flutter 3 stub

---
+++ bitcoin_ticker/lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'price_screen.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: Colors.blue,
+          // brightness: Brightness.dark,
+        ),
+        useMaterial3: true,
+      ),
+      home: const PriceScreen(),
+    );
+  }
+}
