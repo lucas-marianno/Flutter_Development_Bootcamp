ea5332ddbcc4892d612640d44f27c82c44f04a1d
lucas-marianno <lucas.pk16@outlook.com>
2023-07-18 21:26:55 -0300

    todoey first commit

---
+++ todoey/lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:todoey/screens/task_screens.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: TasksScreen(),
+    );
+  }
+}
